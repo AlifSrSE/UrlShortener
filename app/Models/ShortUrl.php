eee031d49f0af15cd7c18177bb8da2b080c31713
Alif Rahman <alif.rahman.c@gmail.com>
2026-08-18 02:04:15 +0600

    short url page

--- app/Models/ShortUrl.php
+++ app/Models/ShortUrl.php
@@ -2,16 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ShortUrl extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'original_url',
         'short_code',
+        'alias',
+        'expires_at',
+        'password',
     ];
-}
 
+    protected $casts = [
+        'expires_at' => 'datetime',
+    ];
+
+    public function clicks(): HasMany
+    {
+        return $this->hasMany(Click::class);
+    }
+
+    public function getShortUrlAttribute(): string
+    {
+        return url($this->alias ?? $this->short_code);
+    }
+
+    public function hasPassword(): bool
+    {
+        return !empty($this->password);
+    }
+
+    public function verifyPassword(string $password): bool
+    {
+        return \Illuminate\Support\Facades\Hash::check($password, $this->password);
+    }
+}
