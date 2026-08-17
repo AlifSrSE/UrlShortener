52f53257f63fe56bd9d6eb10662d117aeec429c2
Alif Rahman <alif.rahman.c@gmail.com>
2026-08-18 02:02:01 +0600

    web route page

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,7 @@ use App\Http\Controllers\UrlShortenerController;
 // });
 Route::get('/', [UrlShortenerController::class, 'index'])->name('home');
 Route::post('/shorten', [UrlShortenerController::class, 'create'])->middleware('throttle:10,1')->name('shorten');
-Route::get('/{code}', [UrlShortenerController::class, 'redirect'])->where('code', '[A-Za-z0-9]{6}');
+Route::post('/password/{code}', [UrlShortenerController::class, 'verifyPassword'])->name('password.verify');
+Route::get('/qr/{code}', [UrlShortenerController::class, 'qrCode'])->name('qr.code');
+Route::post('/confirm/{code}', [UrlShortenerController::class, 'confirm'])->name('confirm');
+Route::get('/{code}', [UrlShortenerController::class, 'redirect'])->where('code', '[A-Za-z0-9_-]{3,30}');
